9365833d4a94970aa4bb1d102e22765e8d86adc4
aryanharitsh <32271976+aryanharitsh@users.noreply.github.com>
2019-10-07 19:11:38 +0530

    Update Quardratic equation.py

--- Quardratic equation.py
+++ Quardratic equation.py
@@ -1,17 +1,8 @@
-# Solve the quadratic equation ax**2 + bx + c = 0
-
-# import complex math module
-import cmath
-
- a = float(input('Enter a: '))
- b = float(input('Enter b: '))
- c = float(input('Enter c: '))
-
-# calculate the discriminant
-d = (b**2) - (4*a*c)
-
-# find two solutions
-sol1 = (-b-cmath.sqrt(d))/(2*a)
-sol2 = (-b+cmath.sqrt(d))/(2*a)
-
-print('The solution are {0} and {1}'.format(sol1,sol2))
+from math import sqrt 
+a=int(input())
+b=int(input())
+c=int(input())
+d=b**2-4*a*c
+d=d**0.5
+print((-b+d)/(2*a))
+print((-b-d)/(2*a))
